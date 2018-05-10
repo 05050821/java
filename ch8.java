87eec7cc641224f2ba6a7fe758860a67d660fd01
05050821 <37134119+05050821@users.noreply.github.com>
2018-05-10 11:24:53 +0800

    Add files via upload

---
+++ ch8.java
@@ -0,0 +1,53 @@
+import java.lang.*;
+
+public class ch8 //主類別
+{
+	public static void main(String args[])
+	{
+		CShape A[] = new CShape[3];
+		A[0]=new CTriangle();
+		A[1]=new CRect();
+		A[2]=new CPentagon();
+		int i;
+		for(i=0;i<3;i++)
+		{
+			A[i].angle();
+			System.out.println(A[i].toString());
+		}		
+	} 
+}
+abstract class CShape
+{
+	public int number;    
+	public int totalAngle; 
+	public abstract void angle();
+	public String toString()
+	{
+		return "正" + number + "邊形的每一角度為" + (totalAngle/number) + "度";
+	}
+}
+class CTriangle extends CShape
+{
+	@Override public void angle()
+	{
+		number=3;
+		totalAngle=(number-2)*180;
+	}
+}
+class CRect extends CShape
+{
+	@Override public void angle()
+	{
+		number=4;
+		totalAngle=(number-2)*180;
+	}
+}
+class CPentagon extends CShape
+{
+	@Override public void angle()
+	{
+		number=5;
+		totalAngle=(number-2)*180;
+	}
+	
+}
